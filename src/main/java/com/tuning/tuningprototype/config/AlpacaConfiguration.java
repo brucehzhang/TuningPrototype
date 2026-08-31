1a7b75196f2f9ca6a0b611ab1e01d489f651a879
brucehzhang <46877284+brucehzhang@users.noreply.github.com>
2026-08-31 22:10:32 +0900

    Spring APCA vars, startup probe threshold increase (#14)

--- src/main/java/com/tuning/tuningprototype/config/AlpacaConfiguration.java
+++ src/main/java/com/tuning/tuningprototype/config/AlpacaConfiguration.java
@@ -3,15 +3,22 @@ package com.tuning.tuningprototype.config;
 import markets.alpaca.client.AlpacaClient;
 import markets.alpaca.client.AlpacaCredentials;
 import markets.alpaca.client.TradingApiEnvironment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AlpacaConfiguration {
 
+    @Value("${APCA_TRADING_KEY_ID}")
+    private String APCA_TRADING_KEY_ID;
+
+    @Value("${APCA_TRADING_SECRET_KEY}")
+    private String APCA_TRADING_SECRET_KEY;
+
     @Bean("getAlpacaClient")
     public AlpacaClient getAlpacaClient() {
-        AlpacaCredentials credentials = AlpacaCredentials.fromTradingApiEnvironmentVariables();
+        AlpacaCredentials credentials = new AlpacaCredentials(APCA_TRADING_KEY_ID, APCA_TRADING_SECRET_KEY);
         return AlpacaClient.builder(credentials)
                 .tradingEnvironment(TradingApiEnvironment.PAPER)
                 .build();
